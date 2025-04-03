846004de479ecbce9db7b1e8f5b1f48fa831027b
Zane Rice <zanetrice@gmail.com>
2025-04-03 03:56:57 -0400

    Use maxheight for CRUD Box so that it doesn't look awkward for less items.

--- src/components/ui/crud_table.tsx
+++ src/components/ui/crud_table.tsx
@@ -126,7 +126,7 @@ export default function CRUDTable<T extends object>(
             />
           </Box>
         </Stack>
-        <Box overflowX="auto" overflowY="auto" height="32rem">
+        <Box overflowX="auto" overflowY="auto" maxHeight="32rem">
           <DataTable
             records={records}
             style={{}}

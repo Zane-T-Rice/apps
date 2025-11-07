f67bc081fb1d10fb5eaee7da1b3787fb26823aac
Zane-T-Rice <zanetrice@gmail.com>
2025-11-07 04:58:25 -0500

    Way better idea than the switches that take up space. Using bgColor of icon.

--- src/components/ui/status_switch.tsx
+++ src/components/ui/status_switch.tsx
@@ -56,19 +56,14 @@ export function StatusSwitch(props: {
       }}
     >
       <Switch.HiddenInput />
-      <Switch.Control
-        bg={{
-          base: checked ? color : "grey",
-          _dark: checked ? color : "grey",
-        }}
-      />
       <Switch.Label>
         <Tooltip content={status}>
           <Image
             src={`${status.toLowerCase()}.png`}
-            width="6"
-            height="6"
+            width="8"
+            height="8"
             alt={status}
+            bgColor="whiteAlpha.600"
           />
         </Tooltip>
       </Switch.Label>

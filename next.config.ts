033d128f20e7a46b69e58b8dfdecefc0ebd40d8c
Zane-T-Rice <zanetrice@gmail.com>
2025-11-25 12:20:49 -0500

    Build FE app as static, because it is.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   /* config options here */
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-  basePath: "/apps",
-  assetPrefix: "/apps",
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
